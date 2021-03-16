f650c35b7e16f0e60e4654c78efc3f8c12a1c209
Andi Neck <andi.neck@intesso.com>
2021-03-16 21:57:09 +0100

    add ohmybash

---
+++ install_apps.sh
@@ -0,0 +1,5 @@
+
+
+sudo apt-get install fonts-powerline --yes
+
+[[ ! -d ~/.oh-my-bash ]] && git clone git://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash || echo "oh-my-bash is already installed"
